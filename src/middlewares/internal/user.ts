8b16ca1176e397c49e9b243c297dc46240b0735f
bakjonghun <bakjonghun@hikick.kr>
2021-10-05 16:37:11 +0900

    remove: CANNOT_FIND_USER

--- src/middlewares/internal/user.ts
+++ src/middlewares/internal/user.ts
@@ -1,40 +1,24 @@
 import dayjs from 'dayjs';
-import {
-  getAccountsClient,
-  logger,
-  RESULT,
-  Wrapper,
-  WrapperCallback,
-} from '../..';
+import { getAccountsClient, Wrapper, WrapperCallback } from '../..';
 
 export function InternalUserMiddleware(): WrapperCallback {
   const accountsClient = getAccountsClient();
 
   return Wrapper(async (req, res, next) => {
-    try {
-      const { userId } = req.params;
-      if (typeof userId !== 'string') throw new Error();
-      const { user } = await accountsClient.get(`users/${userId}`).json();
+    const { userId } = req.params;
+    if (typeof userId !== 'string') throw new Error();
+    const { user } = await accountsClient.get(`users/${userId}`).json();
+    req.internal.user = {
+      userId: user.userId,
+      realname: user.realname,
+      phoneNo: user.phoneNo,
+      email: user.email,
+      birthday: dayjs(user.birthday),
+      usedAt: dayjs(user.usedAt),
+      createdAt: dayjs(user.createdAt),
+      updatedAt: dayjs(user.updatedAt),
+    };
 
-      req.internal.user = {
-        userId: user.userId,
-        realname: user.realname,
-        phoneNo: user.phoneNo,
-        email: user.email,
-        birthday: dayjs(user.birthday),
-        usedAt: dayjs(user.usedAt),
-        createdAt: dayjs(user.createdAt),
-        updatedAt: dayjs(user.updatedAt),
-      };
-
-      next();
-    } catch (err: any) {
-      if (process.env.NODE_ENV !== 'prod') {
-        logger.error(err.message);
-        logger.error(err.stack);
-      }
-
-      throw RESULT.CANNOT_FIND_USER();
-    }
+    next();
   });
 }
